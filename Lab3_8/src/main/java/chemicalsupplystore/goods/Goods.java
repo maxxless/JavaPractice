aa2b2fb6146dc791fd961c7001aa7b592c64d733
Max Leshchyshyn <masyaka35@gmail.com>
2018-03-31 23:54:00 +0300

    lab8

---
+++ Lab3_8/src/main/java/chemicalsupplystore/goods/Goods.java
@@ -0,0 +1,73 @@
+package chemicalsupplystore.goods;
+
+public abstract class Goods {
+
+    private Integer id;
+
+    private String name;
+    private double price;
+    private int amount;
+
+    public Goods(final String name, final double price, final int amount) {
+        setName(name);
+        setPrice(price);
+        setAmount(amount);
+    }
+
+    public Goods() {
+    }
+
+    public String getHeaders() {
+        return "Name" + "," + "Price" + "," + "Amount" + "," + "Type" + "," + "Colour";
+    }
+
+    public String toCVS() {
+        return getName() + "," + getPrice() + "," + getAmount() + "," + getGoodsType() + "," + getGoodsColour();
+    }
+
+    public abstract GoodsType getGoodsType();
+
+    public abstract GoodsColour getGoodsColour();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Empty string is invalid.");
+        }
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(final double price) {
+        if (price < 0.0) {
+            throw new IllegalArgumentException("Negative number is invalid.");
+        }
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(final int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Negative number is invalid.");
+        }
+        this.amount = amount;
+    }
+
+}
